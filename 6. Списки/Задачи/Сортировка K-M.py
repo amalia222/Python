f729de94c91e8105a2e183eaa3e6cfe7bd390d21
Андрей Боев <a.boev.it@gmail.com>
2025-03-28 19:36:13 +0300

    Проверка ДЗ

--- 6. Списки/Задачи/Сортировка K-M.py
+++ 6. Списки/Задачи/Сортировка K-M.py
@@ -1,5 +1,4 @@
 lst = [int(i) for i in input().split()]
-lst1 = [int(i) for i in input().split()]
-k, m, d = lst1
+k, m, d = [int(i) for i in input().split()]
 lst[k - 1 : m] = sorted(lst[k - 1 : m], reverse = d < 0)
 print(*lst)
--- 6. Списки/Задачи/Циклический сдвиг.py
+++ 6. Списки/Задачи/Циклический сдвиг.py
@@ -1,8 +1,12 @@
 def shift_right(lst, st):
     st = st % len(lst)
     return lst[-st:] + lst[:-st]
+
+
 def shift_left(lst, st):
     return shift_right(lst, -st)
+
+
 n = [int(i) for i in input().split()]
 steps = int(input())
 print(shift_right(n, steps))
