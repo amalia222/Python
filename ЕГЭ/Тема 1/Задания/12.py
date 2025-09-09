4c608cdc03e7dffd7aa8d2ec8e43b4f2f757d9da
Андрей Боев <a.boev.it@gmail.com>
2025-09-09 18:45:19 +0300

    UPD Задание 12

---
+++ ЕГЭ/Тема 1/Задания/12.py
@@ -0,0 +1,15 @@
+import hashlib
+
+# Решение
+
+
+
+
+
+
+
+
+
+# Ответ
+answer = ...
+print("Верно" if (hashlib.md5(str(answer).encode()).hexdigest() == 'e369853df766fa44e1ed0ff613f563bd') else "Неверно")
