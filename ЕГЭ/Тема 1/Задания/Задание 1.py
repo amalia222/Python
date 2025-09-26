e0fbfffacb3927a34377ce33ae1952d5e65d6c19
Андрей Боев <a.boev.it@gmail.com>
2025-09-26 18:04:12 +0300

    UPD

--- ЕГЭ/Тема 1/Задания/Задание 1.py
+++ ЕГЭ/Тема 1/Задания/Задание 1.py
@@ -1,11 +1,15 @@
-from itertools import permutations
+# Решение
 
-table = '* * * * * * * * * * * * * * * * * * * * * *'
-graph = 'CF FC FD DF FG GF AF FA BF FB FE EF EB BE BA AB AG GA GD DG DC CD'
 
-for i in permutations('ABCDEFG'):
-    n_graph = table
-    for j in range(1, 8):
-        n_graph = n_graph.replace(str(j), i[j - 1])
-    if set(n_graph.split()) == set(graph.split()):
-        print(i)
+
+
+
+
+
+answer = ...
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(1, 1, answer, '735b90b4568125ed6c3f678819b6e058'))
--- ЕГЭ/Тема 1/Задания/Задание 10.py
+++ ЕГЭ/Тема 1/Задания/Задание 10.py
@@ -1,11 +1,13 @@
-from itertools import permutations
+# Решение
 
-table = '8 8 19 19 11 11 28 28 12 12 20 20 26 26 18 18 29 16 29 16 32 32 15 15 9 9 14 14 17 17'
-graph = 'АБ БА БВ ВБ БИ ИБ БД ДБ ИД ДИ ИЕ ЕИ ИЖ ЖИ ЖЕ ЕЖ ЖГ ГЖ ЖА АЖ АГ ГА АВ ВА ВД ДВ ДЕ ЕД ГЕ ЕГ'
 
-for i in permutations('АБВГДЕЖИ'):
-    n_graph = table
-    for j in range(1, 9):
-        n_graph = n_graph.replace(str(j), i[j - 1])
-    if set(n_graph.split()) == set(graph.split()):
-        print(i)
+
+
+
+answer = ...
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(3, 12, answer, 'aab3238922bcc25a6f606eb525ffdc56'))
