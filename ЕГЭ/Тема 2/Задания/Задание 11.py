2c5826f279edeaf59a0d3be2eed17067194cffa9
Андрей Боев <a.boev.it@gmail.com>
2025-09-30 18:23:51 +0300

    Проверка заданий. Исправление ошибок работы системы

--- ЕГЭ/Тема 2/Задания/Задание 11.py
+++ ЕГЭ/Тема 2/Задания/Задание 11.py
@@ -15,13 +15,14 @@ for a0, a1, a2, a3, a4, a5 in product((0, 1), repeat=6):
         for x, y, z, w in permutations((0, 1, 2, 3)):
             if all([f1(i[x], i[y], i[z], i[w]) == i[-2] and f2(i[x], i[y], i[z], i[w]) == i[-1] for i in s]):
                 print(x, y, z, w)
+
 answer = 'ywxz'
 
 #
 
 import datetime
 import hashlib
-from ЕГЭ.tests.conftest import add_result
+from tests.conftest import add_result
 if answer is not Ellipsis:
     result = 1 if hashlib.md5(str(answer).encode()).hexdigest() == '7379de4777f5748aa568b8d0bf8c3795' else 0
     print("Верно" if result else "Неверно")
--- ЕГЭ/Тема 2/Задания/Задание 2.py
+++ ЕГЭ/Тема 2/Задания/Задание 2.py
@@ -19,7 +19,7 @@ answer = 'wzyx'
 
 import datetime
 import hashlib
-from ЕГЭ.tests.conftest import add_result
+from tests.conftest import add_result
 if answer is not Ellipsis:
     result = 1 if hashlib.md5(str(answer).encode()).hexdigest() == 'e0abee87e4ba1de22c6b8cf076c5016b' else 0
     print("Верно" if result else "Неверно")
