3bc3d072468bd683b28ea0d16440d7dbd4f1d127
Amely <amaliya.maksimenko.09@mail.ru>
2025-10-07 18:14:32 +0300

    Решение задач

--- ЕГЭ/Тема 2/Задания/Test.py
+++ ЕГЭ/Тема 2/Задания/Test.py
@@ -1,6 +1,9 @@
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            for w in range(2):
-                if not((x or not y) <= (w == z) and ((x or not y) == (w <= z))):
-                    print(x, y, z, w)
+from sys import *
+setrecursionlimit(3000)
+def f(n):
+    if n == 1:
+        return 1
+    if n > 1:
+        return n * f(n - 1)
+
+print(((f(2025) // 25) + f(2024)) // f(2023))
--- ЕГЭ/Тема 2/Задания/Тест 2.py
+++ ЕГЭ/Тема 2/Задания/Тест 2.py
@@ -1,16 +1,5 @@
-from itertools import product, permutations
+from itertools import *
 
-
-def f1(x, y, z, w):
-    return 1 if z or z == w or not (y <= x) else 0
-
-
-for a0, a1, a2, a3, a4, a5, a6, in product((0, 1), repeat=7):
-    s = [(a0, a1, 0, 1, 0),
-         (a2, 1, a3, 0, 0),
-         (a4, 0, a5, a6, 0)]
-    if len(set(s)) == len(s):
-        for x, y, z, w in permutations((0, 1, 2, 3)):
-            if all([f1(i[x], i[y], i[z], i[w]) == i[-1] for i in s]):
-                print(f'x = {x + 1}; y = {y + 1}; z = {z + 1}; w = {w + 1}')
-                break
+graph = 'AB BA AC CA AG GA BC CB BD DB CE EC DE ED EG GE DF FD FG GF'
+table = '12 13 17 21 26 27 31 35 36 45 46 47 53 54 62 63 64 71 72 74'
+for
