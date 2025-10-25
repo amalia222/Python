75c90291d29bf992344d2e61095315b53df11932
Amely <amaliya.maksimenko.09@mail.ru>
2025-10-25 15:01:29 +0300

    Решение задач

--- ЕГЭ/Тема 8/Задания/test.py
+++ ЕГЭ/Тема 8/Задания/test.py
@@ -1,7 +1,1 @@
-from itertools import *
-list_val = product('ВЕРОНИКА', repeat = 6)
-count = 0
-for s in list_val:
-    if s.count('Е') + s.count('О') + s.count('И') + s.count('А') > s.count('В') + s.count('Р') + s.count('Н') + s.count('К'):
-        count += 1
-print(count)
+
