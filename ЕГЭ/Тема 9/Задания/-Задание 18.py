bbcfed7ef36f0177a549ba5507e88eaf60caa608
Amely <amaliya.maksimenko.09@mail.ru>
2025-10-28 15:46:13 +0300

    Обновлен статус задания 18 темы 9. Задание решено Неверно

---
+++ ЕГЭ/Тема 9/Задания/-Задание 18.py
@@ -0,0 +1,9 @@
+# Решение
+
+answer = 12279
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(9, 18, answer, '67388f1834f7d6243b753ec33584a8df'))
