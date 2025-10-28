92434b3cb17ff798b39b285ca8fb046f7bc2965b
Amely <amaliya.maksimenko.09@mail.ru>
2025-10-28 15:02:53 +0300

    Обновлен статус задания 71 темы 7. Задание решено Неверно

---
+++ ЕГЭ/Тема 7/Задания/-Задание 71.py
@@ -0,0 +1,14 @@
+# Решение
+"""
+на 1 экспонат 10 000 000 бит всего после сжатия
+на фото 49 766 400 до сжатия
+после сжатия 9 953 280
+на текст 46 720 бит, 5,84 КБайт, примерно 6 КБайт
+"""
+answer = 6
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(7, 71, answer, 'fc490ca45c00b1249bbe3554a4fdf6fb'))
