30ca86c603af2a5da4c60856cee57fe9b94c332d
Amely <amaliya.maksimenko.09@mail.ru>
2025-10-29 13:45:53 +0300

    Обновлен статус задания 1 темы 1. Задание решено Верно

--- ЕГЭ/Тема 1/Задания/+Задание 1.py
+++ ЕГЭ/Тема 1/Задания/+Задание 1.py
@@ -1,12 +1,12 @@
 # Решение
 from itertools import permutations
 
-table = '15 16 18 23 26 32 34 37 43 46 48 51 57 58 61 62 64 73 75 81 84 85'
-graph = 'АБ БА БЕ ЕБ КЕ ЕК КД ДК ДЖ ЖД ЖА АЖ АГ ГА ГБ БГ ГВ ВГ ВЕ ЕВ ВД ДВ'
+table = '14 17 25 26 34 36 41 43 47 52 56 57 62 63 65 71 74 75'
+graph = 'АБ БА АВ ВА БГ ГБ БД ДБ ВК КВ ВЕ ЕВ ГД ДГ ДЕ ЕД ЕК КЕ'
 
-for p in permutations('АБВГДЕЖК'):
+for p in permutations('АБВГДЕК'):
     new_graph = table
-    for i in range(1, 9):
+    for i in range(1, 8):
         new_graph = new_graph.replace(str(i), p[i - 1])
     if set(new_graph.split()) == set(graph.split()):
         print(p)
