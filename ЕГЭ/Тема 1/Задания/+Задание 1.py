989e345fe3ffab551857dbf41951ab37ed220414
Amely <amaliya.maksimenko.09@mail.ru>
2025-10-29 13:36:51 +0300

    Обновлен статус задания 1 темы 1. Задание решено Верно

--- ЕГЭ/Тема 1/Задания/+Задание 1.py
+++ ЕГЭ/Тема 1/Задания/+Задание 1.py
@@ -1,11 +1,15 @@
 # Решение
+from itertools import permutations
 
+table = '12 13 14 15 16 17 21 27 31 35 37 41 45 46 51 53 54 61 64 71 72 73'
+graph = 'AC CA CG GC CF FC CE EC DC CD CB BC BD DB DE ED EF FE FG GF GA AG'
 
-
-
-
-
-
+for p in permutations('ABCDEFG'):
+    new_graph = table
+    for i in range(1, 8):
+        new_graph = new_graph.replace(str(i), p[i - 1])
+    if set(new_graph.split()) == set(graph.split()):
+        print(p)
 answer = 67
 
 #
--- ЕГЭ/Тема 7/Задания/Задание 71.py
+++
@@ -1,9 +0,0 @@
-# Решение
-10485760
-answer = ...
-
-#
-
-from tests.conftest import result_register
-if answer is not Ellipsis:
-    print(result_register(7, 71, answer, 'fc490ca45c00b1249bbe3554a4fdf6fb'))
