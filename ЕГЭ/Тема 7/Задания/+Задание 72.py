866a73fe6cd2bcf8da499581e82978cb11262715
Amely <amaliya.maksimenko.09@mail.ru>
2025-12-26 17:41:06 +0300

    Обновлен статус задания 72 темы 7. Задание решено Верно

---
+++ ЕГЭ/Тема 7/Задания/+Задание 72.py
@@ -0,0 +1,14 @@
+# Решение
+"""
+61440 секунд длилась запись
+до сжатия 7500 мб - 100%
+после сжатия 1500 мб - х%
+х% = 20% как объем сжатой книги от первоначальной
+"""
+answer = 80
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(7, 72, answer, 'f033ab37c30201f73f142449d037028d'))
