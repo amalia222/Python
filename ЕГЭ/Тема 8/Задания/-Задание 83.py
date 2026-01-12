737ad0457bd38b82e010cfc5a858ffbb5d74fed3
Amely <amaliya.maksimenko.09@mail.ru>
2026-01-12 22:37:32 +0300

    Обновлен статус задания 83 темы 8. Задание решено Неверно

---
+++ ЕГЭ/Тема 8/Задания/-Задание 83.py
@@ -0,0 +1,10 @@
+from itertools import *
+list_val = list(product('БКФЦ', repeat = 5))
+print(list_val[239])
+answer = 'БЦФЦЦ'
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(8, 83, answer, 'f0a551e113a7af86f780ae14e74c3ac7'))
