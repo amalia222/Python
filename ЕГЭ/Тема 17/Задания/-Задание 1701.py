868841ad6d03d53ab88131415cf679f4159f0280
Amely <amaliya.maksimenko.09@mail.ru>
2026-01-20 19:04:22 +0300

    Обновлен статус задания № 1701 Тема: 17 > Неверно

--- ЕГЭ/Тема 17/Задания/-Задание 1701.py
+++ ЕГЭ/Тема 17/Задания/-Задание 1701.py
@@ -1,10 +1,10 @@
-list_val = [int(x) for x in open('Задание 1701.txt')]
+list_val = [int(x) for x in open('-Задание 1701.txt')]
 count_pair = 0
 min_sum = 10 ** 9
 max9 = max(list_val, key = lambda x: -10000 < x < -1000 and abs(x) % 9 == 0)
 print(max9)
 for i in range(len(list_val) - 1):
-    if list_val[i] < 0 < list_val[i + 1] or list_val[i + 1] < 0 < list_val[i]:
+    if list_val[i] * list_val[i + 1] < 0:
         if list_val[i] + list_val[i + 1] > max9:
             count_pair += 1
             if (list_val[i]) ** 2 + (list_val[i + 1]) ** 2 < min_sum:
