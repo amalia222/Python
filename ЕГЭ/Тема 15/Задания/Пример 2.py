97444f8ceb2968647b20340adc7c43928590e1d0
Андрей Боев <a.boev.it@gmail.com>
2026-01-20 18:13:36 +0300

    NEW: Новые задания

--- ЕГЭ/Тема 15/Задания/Пример 2.py
+++
@@ -1,3 +0,0 @@
-for a in range(1, 1000):
-    if all((y > a) or (152 != 2 * y + 3 * x) or (a < x) for x in range(1000) for y in range(1000)):
-        print(a)
--- ЕГЭ/Тема 16/Задания/Пример 1.py
+++
@@ -1,9 +0,0 @@
-f = [0] * 1_600_000_000
-count = 0
-for i in range(1, len(f)):
-    f[i] = f[i // 10] + i % 10
-
-for n in range(765432015, 154261340):
-    if f[n] > f[n + 1]:
-        count += 1
-print(count)
