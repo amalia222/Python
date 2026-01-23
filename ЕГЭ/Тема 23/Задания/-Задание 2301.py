9aa37e76a63a81ac726ab5975c70bbcb6abfb939
Amely <amaliya.maksimenko.09@mail.ru>
2026-01-23 18:25:51 +0300

    Обновлен статус задания № 2301 Тема: 23 > Неверно

---
+++ ЕГЭ/Тема 23/Задания/-Задание 2301.py
@@ -0,0 +1,14 @@
+def f(x, y):
+    if y == x:
+        return 1
+    if x > y:
+        return 0
+    return f(x + 1, y) + f(x * 2, y) + f(x * 3, y)
+print((f(9, 24) * f(24, 81)) + (f(9, 27) * f(27, 81)))
+answer = 222
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(23, 2301, answer, 'a8baa56554f96369ab93e4f3bb068c22'))
