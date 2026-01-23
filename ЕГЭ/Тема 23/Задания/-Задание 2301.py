7477b6829fbacc38e6346e18736606d16fe79b74
Amely <amaliya.maksimenko.09@mail.ru>
2026-01-23 18:33:55 +0300

    Обновлен статус задания № 2301 Тема: 23 > Неверно

--- ЕГЭ/Тема 23/Задания/-Задание 2301.py
+++ ЕГЭ/Тема 23/Задания/-Задание 2301.py
@@ -1,11 +1,19 @@
-def f(x, y):
+def f1(x, y):
     if y == x:
         return 1
-    if x > y:
+    if x > y or x == 27:
         return 0
-    return f(x + 1, y) + f(x * 2, y) + f(x * 3, y)
-print((f(9, 24) * f(24, 81)) + (f(9, 27) * f(27, 81)))
-answer = 222
+    return f1(x + 1, y) + f1(x * 2, y) + f1(x * 3, y)
+
+def f2(x, y):
+    if y == x:
+        return 1
+    if x > y or x == 24:
+        return 0
+    return f2(x + 1, y) + f2(x * 2, y) + f2(x * 3, y)
+
+print((f1(9, 24) * f1(24, 81)) + (f2(9, 27) * f2(27, 81)))
+answer = 62
 
 #
 
