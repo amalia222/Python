cf907b57bca37a516eb544e184b2965876e85ef6
Андрей Боев <a.boev.it@gmail.com>
2026-01-27 18:06:20 +0300

    NEW: Тема 24

---
+++ ЕГЭ/Тема 24/Задания/Задание 2401.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+
+answer = ...
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(24, 2401, answer, '0e65972dce68dad4d52d063967f0a705'))
---
+++ ЕГЭ/Тема 24/Задания/Задание 2402.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+
+answer = ...
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(24, 2402, answer, '0bb01cf1951560939ce9d92c72dfd974'))
