e704b18a04877afbabdf8e5dab4a8e24516e826c
Андрей Боев <a.boev.it@gmail.com>
2026-02-06 18:12:53 +0300

    Обновлен статус задания № 82 Тема: 8 > Верно

---
+++ ЕГЭ/Тема 8/Задания/+Задание 82.py
@@ -0,0 +1,17 @@
+from itertools import *
+list_val = list(product('АКОРСТ', repeat = 5))
+max_index = 0
+for i in range(len(list_val)):
+    if (i + 1) % 2 == 0:
+        s = ''.join(list_val[i])
+        if s.count('О') == 2:
+            if s[0] !='А' and s[0] !='С' and s[0] !='Т':
+                max_index = i
+print(max_index + 1)
+answer = 5058
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(8, 82, answer, '7ffb4e0ece07869880d51662a2234143'))
