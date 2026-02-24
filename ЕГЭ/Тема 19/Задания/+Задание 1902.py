14e891bf48bf982101b4a709c9ae9a3edfcd93f8
Amely <amaliya.maksimenko.09@mail.ru>
2026-02-24 22:37:22 +0300

    Обновлен статус задания № 1902 Тема: 19 > Верно

---
+++ ЕГЭ/Тема 19/Задания/+Задание 1902.py
@@ -0,0 +1,20 @@
+def game(heap, moves, to):
+    if heap >= 112:
+        return moves % 2 == to % 2
+    if moves == to:
+        return 0
+    s = [1, heap]
+    for i in range(2, heap):
+        if heap % i == 0:
+            s.append(i)
+    h = [game(heap + i, moves + 1, to) for i in s]
+    return any(h) if (moves + 1) % 2 == to % 2 else all(h)
+print([s for s in range(1, 111) if game(s, 0, 2) and not game(s, 0, 1)])
+print(game(28, 0, 2))
+answer = 55
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(19, 1902, answer, 'b53b3a3d6ab90ce0268229151c9bde11'))
