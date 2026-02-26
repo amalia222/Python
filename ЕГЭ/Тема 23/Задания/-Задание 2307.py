65d343cdb9ba3959f48ab91845d156dc287b2e2a
Amely <amaliya.maksimenko.09@mail.ru>
2026-02-26 20:46:49 +0300

    Обновлен статус задания № 2307 Тема: 23 > Неверно

--- ЕГЭ/Тема 23/Задания/-Задание 2307.py
+++ ЕГЭ/Тема 23/Задания/-Задание 2307.py
@@ -1,12 +1,13 @@
 def f(x, y, k = 0):
     if x == y:
         return 1
-    if x > y or k > 2:
+    if x > y or k > 1:
         return 0
     return f(x + 1, y, k) + f(x + 2, y, k) + f(x * 2, y, k + 1) + f(x * 3, y, k + 1)
 
 print(f(1, 10))
-answer = 196
+print(1 > 1)
+answer = 157
 
 #
 
