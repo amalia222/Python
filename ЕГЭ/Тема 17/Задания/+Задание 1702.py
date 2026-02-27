822dbe5ec8adce59f66e88e75de5a63e892275b2
Amely <amaliya.maksimenko.09@mail.ru>
2026-02-27 18:35:56 +0300

    Обновлен статус задания № 1702 Тема: 17 > Верно

--- ЕГЭ/Тема 17/Задания/+Задание 1702.py
+++ ЕГЭ/Тема 17/Задания/+Задание 1702.py
@@ -1,14 +1,13 @@
-list_val = list(int(x) for x in open('Задание 1702.txt'))
+list_val = list(int(x) for x in open('+Задание 1702.txt'))
 count_pair = 0
 max_sum = 0
 for i in range(len(list_val) - 1):
-    for j in range(len(list_val) - 1):
-        if i > j:
-            if abs(list_val[i] - list_val[j]) % 60 == 0:
-                if list_val[i] % 15 == 0 or list_val[j] % 15 == 0:
-                    count_pair += 1
-                    if list_val[i] - list_val[j] > max_sum:
-                        max_sum = list_val[i] - list_val[j]
+    for j in range(i + 1, len(list_val) - 1):
+        if abs(list_val[i] - list_val[j]) % 60 == 0:
+            if list_val[i] % 15 == 0 or list_val[j] % 15 == 0:
+                count_pair += 1
+                if list_val[i] - list_val[j] > max_sum:
+                    max_sum = list_val[i] - list_val[j]
 print(count_pair, max_sum)
 answer1 = 63517
 answer2 = 9960
