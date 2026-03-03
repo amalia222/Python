31d9c63b564a72075a1ee972d249b7af1f9dc1bc
Amely <amaliya.maksimenko.09@mail.ru>
2026-03-03 18:06:42 +0300

    Решение задач

--- ЕГЭ/Тема 15/Задания/Задание 1506.py
+++ ЕГЭ/Тема 15/Задания/Задание 1506.py
@@ -1,11 +1,14 @@
-# Решение
-
-
-
-
-
-
-
+p = range(167242, 514210)
+q = range(403149, 718530)
+r = range(522897, 816282)
+s = []
+for x in range(167242, 816282):
+    for y in range(x + 1, 816282 + 1):
+        a = range(x, y)
+        if all((i in q) <= (((i in p) or (i in r)) <= (i in a)) for i in range(1, 100)):
+            s.append(len(a))
+            print(len(a))
+print(min(s))
 answer = ...
 
 #
