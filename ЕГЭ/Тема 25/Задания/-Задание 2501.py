01456f66c4813e6833366f8a22b913699d571f90
Amely <amaliya.maksimenko.09@mail.ru>
2026-03-03 18:39:27 +0300

    Обновлен статус задания № 2501 Тема: 25 > Неверно

---
+++ ЕГЭ/Тема 25/Задания/-Задание 2501.py
@@ -0,0 +1,11 @@
+from fnmatch import fnmatch
+for i in range(4104676, 10 ** 10, 2026):
+    if fnmatch(str(i), '5?34?71*2'):
+        print(i)
+answer = [...]
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(25, 2501, answer, 'ac788180ab5a2f5b1ff54976b883276a'))
