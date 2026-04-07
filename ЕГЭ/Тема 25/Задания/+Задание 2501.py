5c811535add090bdd104cb75eae9f220029360a3
Amely <amaliya.maksimenko.09@mail.ru>
2026-04-07 18:37:48 +0300

    Обновлен статус задания № 2501 Тема: 25 > Верно

---
+++ ЕГЭ/Тема 25/Задания/+Задание 2501.py
@@ -0,0 +1,18 @@
+from fnmatch import fnmatch
+for i in range(2026, 10 ** 10, 2026):
+    i = str(i)
+    if fnmatch(i, '5?34?71*2'):
+        if i[1] in '13579' and i[4] in '13579':
+            print(i)
+
+answer = [553497122,
+5134171692,
+5134971962,
+5734171592,
+5734971862]
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(25, 2501, answer, 'ac788180ab5a2f5b1ff54976b883276a'))
