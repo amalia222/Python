baf7c2a43637d6beef7f0fbb74b9eef04089904d
Amely <amaliya.maksimenko.09@mail.ru>
2026-04-30 19:09:22 +0300

    Решение задач

--- ЕГЭ/Тема 5/Задания/test.py
+++ ЕГЭ/Тема 5/Задания/test.py
@@ -1,4 +1,18 @@
-for x in range(1, 100000, 10):
-    for y in range(1, 100000, 10):
-        if 400 * x ** 2  + 500 * y ** 2  == 7200000:
-            print(x, y, x + y)
+def prime_factors(n):
+    factors = []
+    d = 2
+    while d * d <= n:
+        while n % d == 0:
+            factors.append(d)
+            n //= d
+        d += 1
+    if n > 1:
+        factors.append(n)
+    return factors
+
+
+for i in range(8996452, 9000000):
+    s = prime_factors(i)
+    if len(s) == 2:
+        print(i, max(s))
+
