e8125749f400641772a12b7ec43d5a6d0c101f47
Amely <amaliya.maksimenko.09@mail.ru>
2026-05-22 18:46:00 +0300

    Обновлен статус задания № 82 Тема: 8 > Неверно

--- ЕГЭ/Тема 8/Задания/-Задание 82.py
+++ ЕГЭ/Тема 8/Задания/-Задание 82.py
@@ -1,13 +1,13 @@
 from itertools import *
 list_val = list(product('АКОРСТ', repeat = 5))
-for i in range(len(list_val) - 1, 0, -2):
+for i in range(len(list_val) - 2, 0, -2):
     s = ''.join(list_val[i])
     if s[0] not in 'АСТ' and s.count('О') == 2:
         print(i)
         break
 
-print(list_val[5056], list_val[5057], list_val[5058])
-answer = 5056
+print(list_val[5162])
+answer = 5162
 
 from tests.conftest import result_register
 if answer is not Ellipsis:
