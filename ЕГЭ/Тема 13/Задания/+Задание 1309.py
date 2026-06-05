0ec847be80413235596d6634034455cbd486e6cc
Amely <amaliya.maksimenko.09@mail.ru>
2026-06-05 19:03:23 +0300

    Обновлен статус задания № 1309 Тема: 13 > Верно

---
+++ ЕГЭ/Тема 13/Задания/+Задание 1309.py
@@ -0,0 +1,19 @@
+from ipaddress import *
+max_count = 0
+for mask in range(16, 33):
+    net = ip_network(f'130.0.5.80/{mask}', False)
+    count = 0
+    count1 = f'{net.network_address:b}'.count('1')
+    for i in net:
+        if f'{i:b}'.count('1') == count1:
+            count += 1
+    if count > max_count:
+        max_count = count
+print(max_count)
+answer = 120
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(13, 1309, answer, 'da4fb5c6e93e74d3df8527599fa62642'))
