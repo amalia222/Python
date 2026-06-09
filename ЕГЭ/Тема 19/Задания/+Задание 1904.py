94a3a742842508c15ed580035e594b2f966bd633
Amely <amaliya.maksimenko.09@mail.ru>
2026-06-09 18:29:58 +0300

    Обновлен статус задания № 1904 Тема: 19 > Верно

---
+++ ЕГЭ/Тема 19/Задания/+Задание 1904.py
@@ -0,0 +1,19 @@
+def game(heap, moves, to):
+    if heap <= 20007:
+        return moves % 2 == to % 2
+    if moves == to:
+        return 0
+    h = [game(heap - 2, moves + 1, to),
+         game(heap - 7, moves + 1, to),
+         game(heap // 3, moves + 1, to)]
+    return any(h) if (moves + 1) % 2 == to % 2 else all(h)
+
+print([s for s in range(20008, 100000) if game(s, 0, 2) and not game(s, 0, 1)])
+
+answer = 60025
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(19, 1904, answer, '543a84894716c6c0ed6cabe60aac9945'))
