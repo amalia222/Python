19790f7588f499222bee9387ea32684eab482c78
Amely <amaliya.maksimenko.09@mail.ru>
2026-06-11 10:57:05 +0300

    Обновлен статус задания № 2504 Тема: 25 > Верно

---
+++ ЕГЭ/Тема 25/Задания/+Задание 2504.py
@@ -0,0 +1,38 @@
+def is_simple(n):
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    else:
+        return True
+
+def sum_del(n):
+    s = set()
+    count = 0
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            s.add(i)
+            s.add(n // i)
+    for i in s:
+        if is_simple(i):
+            count += 1
+    return sum(s), count
+
+l = []
+for i in range(4555706, 4600000):
+    if i % 10 != 3:
+        s, k = sum_del(i)
+        if (i - s - k) % 100 == 23 and (i - s - k) > 0:
+            l.append(i)
+            if len(l) == 5:
+                break
+
+print(l)
+
+# Ответ в виде списка чисел []
+answer = l
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(25, 2504, answer, '4f6cb100db828c27d436322ae3bffeef'))
